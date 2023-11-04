e7b243340b8359ff200bdee05f97d648bf1b2189
alibalapour <alibalapour93.ab@gmail.com>
2023-11-03 21:41:18 -0700

    update: update extract_nuclei bash

--- code/extract_nuclei.sh
+++ code/extract_nuclei.sh
@@ -12,13 +12,23 @@ if ! command -v python &> /dev/null; then
     exit 1
 fi
 
-# Clone from hover_net repository
-git clone https://github.com/vqdang/hover_net.git
+# Define the directory where you want to clone the repository
+REPO_LINK="https://github.com/vqdang/hover_net.git"
+
+# Check if the directory already exists
+if [ -d "$REPO_LINK" ]; then
+    echo "The directory '$REPO_LINK' already exists. Skipping cloning."
+else
+    # Directory does not exist, so clone the repository
+    git clone "$REPO_LINK"
+    echo "Repository cloned to '$REPO_LINK'."
+fi
 
 # Set absolute paths for directories and files
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 REPO_DIR="$SCRIPT_DIR/hover_net"
 MODEL_PATH="$SCRIPT_DIR/hovernet_original_consep_notype_tf2pytorch.tar"
+IMG_DIR="$SCRIPT_DIR/imgs"
 
 cd REPO_DIR
 
@@ -28,7 +38,10 @@ pip install -r "$REPO_DIR/requirements.txt"
 # Quick fix: Install a specific version of docopt
 pip install docopt==0.6.2
 
-# Download a file using gdown
+# Install gdown
+pip install gdown
+
+# Download a fine-tuned model using gdown
 gdown https://drive.google.com/uc?id=1BF0GIgNGYpfyqEyU0jMsA6MqcUpVQx0b
 
 # Run the Python script with error handling
@@ -36,7 +49,7 @@ python "$REPO_DIR/run_infer.py" \
     --model_mode=original \
     --model_path="$MODEL_PATH" \
     tile \
-    --input_dir="imgs" \
+    --input_dir="$IMG_DIR" \
     --output="." \
     --draw_dot \
     --save_qupath
