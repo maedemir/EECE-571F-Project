b5c212b85f80885ce82080c87e18d5344dc03bbf
alibalapour <alibalapour93.ab@gmail.com>
2023-11-08 08:15:10 -0800

    update: update extract_nuclei.sh to make it robust

--- code/extract_nuclei.sh
+++ code/extract_nuclei.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
-# Check if Git is available
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Please install Git and retry."
-    exit 1
-fi
+# Set absolute paths for directories and files
+SOURCE_DIR="$( pwd -P )"
+IMG_DIR="$SOURCE_DIR/imgs"
+OUTPUT_DIR="$SOURCE_DIR/outputs"
 
-# Check if Python is available
-if ! command -v python &> /dev/null; then
-    echo "Python is not installed. Please install Python and retry."
-    exit 1
-fi
-
-# Define the directory where you want to clone the repository
+# Define the repositroy link and directory where you want to clone the repository
 REPO_LINK="https://github.com/vqdang/hover_net.git"
+REPO_DIR="$SOURCE_DIR/hover_net"
 
 # Check if the directory already exists
-if [ -d "$REPO_LINK" ]; then
-    echo "The directory '$REPO_LINK' already exists. Skipping cloning."
+if [ -d "$REPO_DIR" ]; then
+    echo "The directory '$REPO_DIR' already exists. Skipping cloning."
 else
     # Directory does not exist, so clone the repository
     git clone "$REPO_LINK"
-    echo "Repository cloned to '$REPO_LINK'."
+    echo "Repository cloned to '$REPO_DIR'."
 fi
-
-# Set absolute paths for directories and files
-SCRIPT_DIR="$( pwd -P )"
-REPO_DIR="$SCRIPT_DIR/hover_net"
-MODEL_PATH="$SCRIPT_DIR/hovernet_original_consep_notype_tf2pytorch.tar"
-IMG_DIR="$SCRIPT_DIR/imgs"
 
 cd REPO_DIR
 
-# Install Python dependencies
-pip install -r "$REPO_DIR/requirements.txt"
+# # Install Python dependencies
+# pip install -r "$REPO_DIR/requirements.txt"
 
-# Quick fix: Install a specific version of docopt
-pip install docopt==0.6.2
+# # Quick fix: Install a specific version of docopt
+# pip install docopt==0.6.2
+# # TODO: change requirements.txt
 
-# Install gdown
-pip install gdown
+# # Install gdown
+# pip install gdown
 
 # Download a fine-tuned model using gdown
 gdown https://drive.google.com/uc?id=1BF0GIgNGYpfyqEyU0jMsA6MqcUpVQx0b
+# TODO: set a directory
+
+MODEL_PATH="$SOURCE_DIR/hovernet_original_consep_notype_tf2pytorch.tar"
+
 
 # Run the Python script with error handling
 python "$REPO_DIR/run_infer.py" \
@@ -50,9 +43,9 @@ python "$REPO_DIR/run_infer.py" \
     --model_path="$MODEL_PATH" \
     tile \
     --input_dir="$IMG_DIR" \
-    --output="." \
-    --draw_dot \
-    --save_qupath
+    --output="$OUTPUT_DIR" 
+    
+# TODO: set output path as argument
 
 # Check the return code of the Python script
 if [ $? -ne 0 ]; then
@@ -61,3 +54,5 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Script execution completed successfully."
+
+# TODO: give classification output
