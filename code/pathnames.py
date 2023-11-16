a6b0832ccfb9232e56ed440b2a26bbecc6d148b2
davidzhc <davidchen14855@gmail.com>
2023-11-15 18:20:18 -0800

    Update pathnames.py

--- code/pathnames.py
+++ code/pathnames.py
@@ -1,7 +1,16 @@
 from os import walk
+
 def walker(root_path):
     # example: root_path = "/Users/dchen2/Desktop/db/"
     with open("paths.txt", 'w') as fp:  
         for (dirpath, dirnames, filenames) in walk(root_path):
                 for j in filenames:
                     fp.writelines(dirpath+j+'\n')
+
+def walker_arr(root_path):
+    # example: root_path = "/Users/dchen2/Desktop/db/"
+    output = [] 
+    for (dirpath, dirnames, filenames) in walk(root_path):
+            for j in filenames:
+                output.append(dirpath+j)
+    return output
