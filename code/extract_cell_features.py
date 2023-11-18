9f9e3efc6f67bc98b3f426f1f9786f600642117b
alibalapour <alibalapour93.ab@gmail.com>
2023-11-17 23:34:47 -0800

    update: split extracting cell features and cells & refactor extract_cell_features

--- code/extract_cell_features.py
+++ code/extract_cell_features.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import torch
 from sklearn.decomposition import PCA
 from torch.utils.data import DataLoader
+from collections import defaultdict
 
 from utils import *
 
@@ -23,90 +24,49 @@ CLASSES = ['HP', 'NCM', 'SSL', 'TA']
 ENABLE_PCA = False
 
 
-def open_json(json_path):
-    with open(json_path) as f:
-        return json.load(f)
-
-
-def get_bboxes():
-    bbox_per_file = {}
-    for cls in CLASSES:
-        cls_json_dir_path = os.path.join(JSON_DIR_PATH, cls)
-        for json_file_name in os.listdir(cls_json_dir_path):
-            json_path = os.path.join(cls_json_dir_path, json_file_name)
-            json_content = open_json(json_path)
-
-            bbox_values = {}
-            for key, value in json_content.get("nuc", {}).items():
-                bbox = value.get("bbox")
-                if bbox is not None:
-                    bbox_values[key] = bbox
-
-            bbox_per_file[json_file_name] = bbox_values
-    return bbox_per_file
-
-
 def get_class_name(file_name):
     class_name = file_name.split('-')[-1].split('_')[0]
     return class_name
 
 
-def extract_cells(bboxes, image_name, image_class):
-    output_path = os.path.join(CELL_IMAGE_PATCHES_DIR, image_class, image_name)
-    os.makedirs(output_path, exist_ok=True)
-
-    image_path = os.path.join(IMAGE_PATH, image_class, image_name)
-    img = Image.open(image_path + '.png')
-
-    for node_name, ((x1, y1), (x2, y2)) in bboxes.items():
-        # Extract patch from the image
-        patch = img.crop((y1, x1, y2, x2))
-
-        # Save the patch to the output folder
-        patch_path = os.path.join(
-            output_path, f'{image_name}-patch_{node_name.zfill(4)}.png')
-        patch.save(patch_path)
-
-
-if __name__ == '__main__':
-    os.makedirs(CELL_IMAGE_PATCHES_DIR, exist_ok=True)
-    os.makedirs(FEATURES_OUTPUT_DIR, exist_ok=True)
-
-    bboxes_per_file = get_bboxes()
-    for file_name, bboxes in bboxes_per_file.items():
-        image_name = file_name.split('/')[-1].split('.')[0]
-        image_class = get_class_name(image_name)
-        extract_cells(bboxes=bboxes, image_name=image_name,
-                      image_class=image_class)
-
+def init_directories():
     for class_name in CLASSES:
-        class_dir = os.path.join(CELL_IMAGE_PATCHES_DIR, class_name)
+        os.path.join(CELL_IMAGE_PATCHES_DIR, class_name)
 
         # Create a folder for each class in nuclei features directory
         os.makedirs(os.path.join(FEATURES_OUTPUT_DIR,
                     class_name), exist_ok=True)
 
-        for patch_name in os.listdir(class_dir):
+        patch_features_dir = os.path.join(
+            FEATURES_OUTPUT_DIR, class_name, 'raw')
+        os.makedirs(patch_features_dir, exist_ok=True)
 
-            patch_cells_dir = os.path.join(class_dir, patch_name)
 
-            dataset = InferenceDataset(
-                path=patch_cells_dir, image_size=(32, 32))
-            test_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1,
-                                         pin_memory=True)
+if __name__ == '__main__':
+    dataset = InferenceDataset(
+        path=CELL_IMAGE_PATCHES_DIR, image_size=(224, 224))
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1,
+                            pin_memory=True)
 
-            vectors = extract_features(
-                test_dataloader, len(os.listdir(patch_cells_dir)))
+    vectors, patch_names = extract_features(dataloader, len(dataset))
+    result_dict = defaultdict(lambda: torch.tensor([]))
 
-            if ENABLE_PCA:
-                pca = PCA()
-                Xt = pca.fit_transform(vectors)
-                plot = plt.scatter(Xt[:, 0], Xt[:, 1])
-                plt.legend(handles=plot.legend_elements()[0])
-                plt.show()
+    for key, value in zip(patch_names, vectors):
+        result_dict[key] = torch.cat([result_dict[key], torch.tensor([value])])
 
-            print(Xt.shape)
+    init_directories()
 
-            patch_features_dir = os.path.join(FEATURES_OUTPUT_DIR, class_name)
-            torch.save(vectors, os.path.join(
-                patch_features_dir, f'{patch_name}-features.pt'))
+    for patch_name, features in result_dict.items():
+        if ENABLE_PCA:
+            pca = PCA()
+            Xt = pca.fit_transform(features)
+            plot = plt.scatter(Xt[:, 0], Xt[:, 1])
+            plt.legend(handles=plot.legend_elements()[0])
+            plt.savefig(patch_name + '.png')
+            plt.close()
+
+        class_name = get_class_name(patch_name)
+        patch_features_dir = os.path.join(
+            FEATURES_OUTPUT_DIR, class_name, 'raw')
+        torch.save(features, os.path.join(
+            patch_features_dir, f'{patch_name}-features.pt'))
---
+++ code/extract_cells.py
@@ -0,0 +1,67 @@
+import os
+import json
+import PIL.Image as Image
+
+
+IMAGE_PATH = 'data/imgs'
+CELL_IMAGE_PATCHES_DIR = 'data/extracted_cells'
+FEATURES_OUTPUT_DIR = 'data/nuclei_features'
+CLASSES = ['HP', 'NCM', 'SSL', 'TA']
+JSON_DIR_PATH = 'data/json'
+
+
+def open_json(json_path):
+    with open(json_path) as f:
+        return json.load(f)
+
+
+def get_bboxes():
+    bbox_per_file = {}
+    for cls in CLASSES:
+        cls_json_dir_path = os.path.join(JSON_DIR_PATH, cls)
+        for json_file_name in os.listdir(cls_json_dir_path):
+            json_path = os.path.join(cls_json_dir_path, json_file_name)
+            json_content = open_json(json_path)
+
+            bbox_values = {}
+            for key, value in json_content.get("nuc", {}).items():
+                bbox = value.get("bbox")
+                if bbox is not None:
+                    bbox_values[key] = bbox
+
+            bbox_per_file[json_file_name] = bbox_values
+    return bbox_per_file
+
+
+def get_class_name(file_name):
+    class_name = file_name.split('-')[-1].split('_')[0]
+    return class_name
+
+
+def extract_cells(bboxes, image_name, image_class):
+    output_path = os.path.join(CELL_IMAGE_PATCHES_DIR, image_class, image_name)
+    os.makedirs(output_path, exist_ok=True)
+
+    image_path = os.path.join(IMAGE_PATH, image_class, image_name)
+    img = Image.open(image_path + '.png')
+
+    for node_name, ((x1, y1), (x2, y2)) in bboxes.items():
+        # Extract patch from the image
+        patch = img.crop((y1, x1, y2, x2))
+
+        # Save the patch to the output folder
+        patch_path = os.path.join(
+            output_path, f'{image_name}-patch_{node_name.zfill(4)}.png')
+        patch.save(patch_path)
+
+
+if __name__ == '__main__':
+    os.makedirs(CELL_IMAGE_PATCHES_DIR, exist_ok=True)
+    os.makedirs(FEATURES_OUTPUT_DIR, exist_ok=True)
+
+    bboxes_per_file = get_bboxes()
+    for file_name, bboxes in bboxes_per_file.items():
+        image_name = file_name.split('/')[-1].split('.')[0]
+        image_class = get_class_name(image_name)
+        extract_cells(bboxes=bboxes, image_name=image_name,
+                      image_class=image_class)
