e8364605b5363685998d6fa8a310f4d0fb0ce000
alibalapour <alibalapour93.ab@gmail.com>
2023-11-18 23:26:10 -0800

    update

--- code/extract_cells.sh
+++ code/extract_cells.sh
@@ -9,7 +9,7 @@ OUTPUT_DIR="$3"
 # Start running cell extraction code
 python "code/extract_cells.py" \
     --image_dir = "$IMG_DIR" \
-    --json_dir = "data/hovernet_output" \
-    --cell_image_patches_dir = "data/extracted_cells"
+    --json_dir = "$HOVERNET_OUTPUT_DIR" \
+    --cell_image_patches_dir = "$OUTPUT_DIR"
     
 
