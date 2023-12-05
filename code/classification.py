8cbc20d10f3b539effabd84157c762af52ed3627
alibalapour <alibalapour93.ab@gmail.com>
2023-12-05 09:02:40 -0800

    update

--- code/classification.py
+++ code/classification.py
@@ -7,12 +7,16 @@ import torch
 from torch_geometric.data import Dataset, download_url, Data, collate
 from torch_geometric.utils import sparse
 from torch_geometric.loader import DataLoader
-from torch_geometric.data import collate
 from torch_geometric.loader import DataLoader
 from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_mean_pool
+import re
+from tqdm import tqdm
+
+
+DEVICE = 'cuda'
 
 
 def get_args():
@@ -60,36 +64,98 @@ class MyOwnDataset(Dataset):
     def processed_file_names(self):
         return [f'data_{idx}.pt' for idx in range(self.size)]
 
-    def process(self):
-        import re
-        pattern = re.compile(r'/([^/]+)/raw/')
+    # def process(self):
 
-        idx = 0
-        for raw_path in self.raw_paths:
-            adj_path = raw_path
-            feature_path = raw_path.replace("graph_adj_matrix", "nuclei_features")[
-                :-3] + '-features.pt'
-            adj = torch.load(raw_path).to_sparse(layout=torch.sparse_coo)
-            # more efficient way to do this???????????
-            adj = sparse.to_edge_index(adj)
-            edge_index = adj[0]  # adj.indices()
-            x = torch.Tensor(torch.load(feature_path))
-            y = self.classes_dict[pattern.search(adj_path).group(1)]
-            data = Data(x=x, edge_index=edge_index, y=y)
+    #     # Create a regex pattern for files with .pt extension
+    #     pt_pattern = re.compile(r".*\.pt$")
 
-            if self.pre_filter is not None and not self.pre_filter(data):
-                continue
+    #     # Create a list to store file paths
+    #     pt_file_paths = []
 
-            if self.pre_transform is not None:
-                data = self.pre_transform(data)
+    #     # Use os.walk to traverse the directory recursively
+    #     for root, dirs, files in os.walk(self.root):
+    #         for file in files:
+    #             # Check if the file matches the regex pattern
+    #             if pt_pattern.match(file):
+    #                 # Get the full path of the file and append it to the list
+    #                 file_path = os.path.join(root, file)
+    #                 pt_file_paths.append(file_path)
 
-            torch.save(data, osp.join(self.processed_dir, f'data_{idx}.pt'))
-            idx += 1
+    #     idx = 0
+    #     temp = []
+    #     prev_slide_name = pt_file_paths[0].split("/")[-2]
+    #     for raw_path in tqdm(pt_file_paths):
+    #         adj_path = raw_path
 
-        # TODO: create super graphs from processed data (after finilazing folder structure)
+    #         cls = raw_path.split("/")[-3]
+    #         feature_path = raw_path.replace("output_graph", "output_cell_features").replace(
+    #             cls+'/'+cls, cls+'/'+cls+'/'+cls)
+    #         patch_name = feature_path.split("/")[-1].split('.')[0]
+    #         slide_name = feature_path.split("/")[-2]
+    #         feature_path = "/".join(feature_path.split("/")
+    #                                 [:-1]) + '/raw/' + patch_name + '-features.pt'
+
+    #         if not os.path.exists(feature_path):
+    #             continue
+
+    #         adj = torch.load(raw_path)
+    #         # adj = torch.load(raw_path).to_sparse(layout=torch.sparse_coo)
+    #         # # more efficient way to do this???????????
+    #         # adj = sparse.to_edge_index(adj)
+    #         if len(adj) == 0:
+    #             continue
+    #         edge_index = adj.T  # adj.indices()
+    #         x = torch.Tensor(torch.load(feature_path))
+    #         # print(x)
+    #         y = self.classes_dict[cls]
+    #         data = Data(x=x, edge_index=edge_index, y=y)
+
+    #         if self.pre_filter is not None and not self.pre_filter(data):
+    #             continue
+
+    #         if self.pre_transform is not None:
+    #             data = self.pre_transform(data)
+
+    #         if prev_slide_name != slide_name:
+    #             new_data, _, _ = collate.collate(Data, temp)
+    #             torch.save(new_data, osp.join(self.processed_dir, f'data_{idx}.pt'))
+    #             idx += 1
+    #             temp = []
+    #             prev_slide_name = slide_name
+
+    #         temp.append(data)
+
+    #     # TODO
+
+    #     # pattern = re.compile(r'/([^/]+)/raw')
+
+    #     # idx = 0
+    #     # for raw_path in self.raw_paths:
+    #     #     adj_path = raw_path
+    #     #     print(self.raw_paths)
+    #     #     feature_path = raw_path.replace("output_graph", "output_cell_features")[
+    #     #         :-3] + '-features.pt'
+    #     #     adj = torch.load(raw_path).to_sparse(layout=torch.sparse_coo)
+    #     #     # # more efficient way to do this???????????
+    #     #     # adj = sparse.to_edge_index(adj)
+    #     #     edge_index = adj[0]  # adj.indices()
+    #     #     x = torch.Tensor(torch.load(feature_path))
+    #     #     y = self.classes_dict[pattern.search(adj_path).group(1)]
+    #     #     data = Data(x=x, edge_index=edge_index, y=y)
+
+    #     #     if self.pre_filter is not None and not self.pre_filter(data):
+    #     #         continue
+
+    #     #     if self.pre_transform is not None:
+    #     #         data = self.pre_transform(data)
+
+    #     #     torch.save(data, osp.join(self.processed_dir, f'data_{idx}.pt'))
+    #     #     idx += 1
+
+    #     # # TODO: c64reate super graphs from processed data (after finilazing folder structure)
 
     def len(self):
-        return len(os.listdir(self.processed_dir))
+        return int(len(os.listdir(self.processed_dir)))
 
     def get(self, idx):
         data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
@@ -99,7 +165,6 @@ class MyOwnDataset(Dataset):
 class GCN(torch.nn.Module):
     def __init__(self, hidden_channels):
         super(GCN, self).__init__()
-        torch.manual_seed(42)  # !!!!!!!!!!!
         self.conv1 = GCNConv(dataset.num_node_features, hidden_channels)
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.conv3 = GCNConv(hidden_channels, hidden_channels)
@@ -123,11 +188,13 @@ class GCN(torch.nn.Module):
         return x
 
 
-def train():
+def train(model, criterion, optimizer, train_loader):
     model.train()
 
-    for data in train_loader:  # Iterate in batches over the training dataset.
+    # Iterate in batches over the training dataset.
+    for data in tqdm(train_loader):
         # Perform a single forward pass.
+        data = data.to(DEVICE)
         out = model(data.x, data.edge_index, data.batch)
         loss = criterion(out, data.y)  # Compute the loss.
         loss.backward()  # Derive gradients.
@@ -135,12 +202,13 @@ def train():
         optimizer.zero_grad()  # Clear gradients.
 
 
-def evaluate(loader, mode='train'):
+def evaluate(model, loader, mode='train'):
     model.eval()
 
     correct = 0
     preds = set()
     for data in loader:  # Iterate in batches over the training/test dataset.
+        data = data.to(DEVICE)
         out = model(data.x, data.edge_index, data.batch)
         pred = out.argmax(dim=1)  # Use the class with highest probability.
         preds.update([int(p) for p in pred])
@@ -156,35 +224,39 @@ def evaluate(loader, mode='train'):
 if __name__ == '__main__':
     args = get_args()
     dataset = MyOwnDataset(
-        root='/content/EECE-571F-Project/data/graph_adj_matrix/')
+        root=args.graph_pairs_path)
 
-    torch.manual_seed(42)
-    # dataset = dataset.shuffle()
+    torch.manual_seed(51)
+    dataset = dataset.shuffle()
 
-    train_dataset = dataset[0::2]
-    test_dataset = dataset[1::2]
+    # train_dataset = dataset[0::2]
+    train_dataset = dataset[:120]
+    # test_dataset = dataset[1::2]
+    test_dataset = dataset[120:150]
 
     print(f'Number of training graphs: {len(train_dataset)}')
     print(f'Number of test graphs: {len(test_dataset)}')
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
-
-    for step, data in enumerate(train_loader):
-        print(f'Step {step + 1}:')
-        print('=======')
-        print(f'Number of graphs in the current batch: {data.num_graphs}')
-        print(data)
-        print()
+    train_loader = DataLoader(
+        train_dataset, batch_size=4, shuffle=True, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=4,
+                             shuffle=True, num_workers=4, pin_memory=True)
+    # for step, data in enumerate(train_loader):
+    #     print(f'Step {step + 1}:')
+    #     print('=======')
+    #     print(f'Number of graphs in the current batch: {data.num_graphs}')
+    #     print()
 
     model = GCN(hidden_channels=64)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     criterion = torch.nn.CrossEntropyLoss()
 
-    for epoch in range(1, 10):
+    for epoch in range(1, 31):
         print(f'Epoch: {epoch:03d}')
-        train()
-        train_acc = evaluate(train_loader, "train")
-        test_acc = evaluate(test_loader, "test")
-        print(
-            f'Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}\n################################################')
+        train(model, criterion, optimizer, train_loader)
+        if epoch % 5 == 0:
+            train_acc = evaluate(model, train_loader, "train")
+            test_acc = evaluate(model, test_loader, "test")
+            print(
+                f'Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}\n################################################')
