60e713f4e945393a857c6b8b26fdb5c003346cb5
alibalapour <alibalapour93.ab@gmail.com>
2023-12-13 12:26:12 -0800

    refactor: refactor extract_cell_features

--- code/extract_cell_features.py
+++ code/extract_cell_features.py
@@ -9,19 +9,10 @@ import argparse
 from utils import *
 
 
-# torch.manual_seed(0)
+VISUALIZE = False
 
 
-ENABLE_PCA = False
-
-
-def init_directories(args, class_name):
-    # Create a folder for each class in nuclei features directory
-    os.makedirs(args.features_output_dir, exist_ok=True)
-    os.makedirs(os.path.join(args.features_output_dir, 'raw'), exist_ok=True)
-    
-
-if __name__ == '__main__':
+def get_parser():
     parser = argparse.ArgumentParser(
         description="Extract features of each cell based on extracted cell images.")
 
@@ -30,24 +21,39 @@ if __name__ == '__main__':
     parser.add_argument("--class_name", default="")
 
     args, unknown = parser.parse_known_args()
-    
-    
-    class_name = args.class_name
 
-    dataset = InferenceDataset(path=args.cell_image_patches_dir, image_size=(224, 224))
+    return args
+
+
+def init_directories(args):
+    # Create a folder for each class in nuclei features directory
+    os.makedirs(args.features_output_dir, exist_ok=True)
+    os.makedirs(os.path.join(args.features_output_dir, 'raw'), exist_ok=True)
+
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    args = get_parser()
+
+    # Create dataset and dataloader
+    dataset = InferenceDataset(
+        path=args.cell_image_patches_dir, image_size=(224, 224))
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1,
                             pin_memory=True)
 
+    # Extract features
     vectors, patch_names = extract_features(dataloader, len(dataset))
     result_dict = defaultdict(lambda: torch.tensor([]))
 
     for key, value in zip(patch_names, vectors):
         result_dict[key] = torch.cat([result_dict[key], torch.tensor([value])])
 
-    init_directories(args, class_name)
+    init_directories(args, args.class_name)
 
+    # Save features
     for patch_name, features in result_dict.items():
-        if ENABLE_PCA:
+        if VISUALIZE:
             pca = PCA()
             Xt = pca.fit_transform(features)
             plot = plt.scatter(Xt[:, 0], Xt[:, 1])
